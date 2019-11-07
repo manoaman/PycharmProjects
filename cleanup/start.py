593ebdec91f56adfc3e4ce21b9e7bee8ec23dfe4
Seita Yamashita <manoaman@gmail.com>
2019-11-06 22:51:41 -0800

    Add cleanup script

--- cleanup/start.py
+++ cleanup/start.py
@@ -2,20 +2,43 @@ import os
 import glob
 
 path = '/Users/seita/Desktop/'
+screenshots = path + 'screenshots'
+pics = path + 'pics'
+pdfs = path + 'pdfs'
 
+# screenshots
 
-# ----------- use os
+if not os.path.isdir(screenshots):
+    os.mkdir(screenshots)
+if not os.path.isdir(pics):
+    os.mkdir(pics)
+if not os.path.isdir(pdfs):
+    os.mkdir(pdfs)
 
 files = []
 # r=root, d=directories, f = files
 for r, d, f in os.walk(path):
     for file in f:
-        if '.png' in file:
+        if 'スクリーンショット' in file:
+            os.rename(os.path.join(r, file), screenshots+'/'+file)
+        elif '.JPG' in file:
+            os.rename(os.path.join(r, file), pics+'/'+file)
+        elif '.jpg' in file:
+            os.rename(os.path.join(r, file), pics+'/'+file)
+        elif '.jpeg' in file:
+            os.rename(os.path.join(r, file), pics+'/'+file)
+        elif '.png' in file:
+            os.rename(os.path.join(r, file), pics+'/'+file)
+        elif '.pdf' in file:
+            os.rename(os.path.join(r, file), pdfs+'/'+file)
+        else:
             files.append(os.path.join(r, file))
 
 for f in files:
     print(f)
 print('--------------------')
+
+
 
 # --------  use glob
 
