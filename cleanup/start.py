d2610aef822272bcd07ccbee71490bafee015350
Seita Yamashita <manoaman@gmail.com>
2019-11-07 22:04:03 -0800

    Comment unused block

--- cleanup/start.py
+++ cleanup/start.py
@@ -6,7 +6,7 @@ screenshots = path + 'screenshots'
 pics = path + 'pics'
 pdfs = path + 'pdfs'
 
-# screenshots
+# organize scattered files
 
 if not os.path.isdir(screenshots):
     os.mkdir(screenshots)
@@ -20,17 +20,17 @@ files = []
 for r, d, f in os.walk(path):
     for file in f:
         if 'スクリーンショット' in file:
-            os.rename(os.path.join(r, file), screenshots+'/'+file)
+            os.rename(os.path.join(r, file), screenshots + '/' + file)
         elif '.JPG' in file:
-            os.rename(os.path.join(r, file), pics+'/'+file)
+            os.rename(os.path.join(r, file), pics + '/' + file)
         elif '.jpg' in file:
-            os.rename(os.path.join(r, file), pics+'/'+file)
+            os.rename(os.path.join(r, file), pics + '/' + file)
         elif '.jpeg' in file:
-            os.rename(os.path.join(r, file), pics+'/'+file)
+            os.rename(os.path.join(r, file), pics + '/' + file)
         elif '.png' in file:
-            os.rename(os.path.join(r, file), pics+'/'+file)
+            os.rename(os.path.join(r, file), pics + '/' + file)
         elif '.pdf' in file:
-            os.rename(os.path.join(r, file), pdfs+'/'+file)
+            os.rename(os.path.join(r, file), pdfs + '/' + file)
         else:
             files.append(os.path.join(r, file))
 
@@ -38,19 +38,17 @@ for f in files:
     print(f)
 print('--------------------')
 
-
-
 # --------  use glob
 
-files = [f for f in glob.glob(path + "**/*.txt", recursive=True)]
-
-for f in files:
-    print(f)
-print('--------------------')
-
-files = [f for f in glob.glob(path + "**/", recursive=True)]
-
-for f in files:
-    print(f)
-
-print('--------------------')
+# files = [f for f in glob.glob(path + "**/*.txt", recursive=True)]
+#
+# for f in files:
+#     print(f)
+# print('--------------------')
+#
+# files = [f for f in glob.glob(path + "**/", recursive=True)]
+#
+# for f in files:
+#     print(f)
+#
+# print('--------------------')
